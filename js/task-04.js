0ff28cc05a2bb799a7e12305ee49c6938c4791e9
Anna Savchuk <anna060411@gmail.com>
2021-07-04 10:38:03 +0300

    task-04 v1.1

--- js/task-04.js
+++ js/task-04.js
@@ -3,8 +3,8 @@ const counterValue = document.querySelector('#value');
 const btnDecrement = document.querySelector('button[data-action="decrement"]');
 const btnIncrement = document.querySelector('button[data-action="increment"]');
 
-const increment = () => Number(counterValue.textContent-=btnDecrement.textContent);
-const decrement = () => Number(counterValue.textContent -= btnIncrement.textContent);
+const increment = () => counterValue.textContent = Number(counterValue.textContent) + Number(btnIncrement.textContent);
+const decrement = () => counterValue.textContent = Number(counterValue.textContent) + Number(btnDecrement.textContent);
 
 btnDecrement.addEventListener('click', decrement);
 btnIncrement.addEventListener('click', increment);
